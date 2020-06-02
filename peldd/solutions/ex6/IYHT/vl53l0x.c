442adc91b728838949391eed155d59b1e75c7a39
sim4036 <56411592+sim4036@users.noreply.github.com>
2020-06-02 15:21:30 +0100

    fixed error check on copy_to_user in solutions for vl53l0x_read

--- peldd/solutions/ex6/IYHT/vl53l0x.c
+++ peldd/solutions/ex6/IYHT/vl53l0x.c
@@ -146,9 +146,12 @@ static ssize_t vl53l0x_read(struct file *file, char __user *buf, size_t count, l
 
 	ret = snprintf(str, sizeof(str), "%d\n", val);
 
-	if (copy_to_user(buf, str, ret))
+	if (ret>count)
+		/* inform user that buf is too small */
+		ret = -EINVAL;
+	else if (copy_to_user(buf, str, ret))
 		ret = -EFAULT;
-
+	
 	return ret;
 }
 
