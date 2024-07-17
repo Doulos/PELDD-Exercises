b023a20d11f2bbd0c004ae217d4605c64b663409
Adrian Thomasset <adrian.thomasset@doulos.com>
2024-07-17 17:10:23 +0100

    Use Yocto SDK toolchain

--- scripts/crosscompilekernel.sh
+++ scripts/crosscompilekernel.sh
@@ -1,3 +1,2 @@
-export ARCH=arm64
-export CROSS_COMPILE=aarch64-linux-gnu-
 export KERNEL_VERSION="5.10.137"
+source $HOME/SDK/environment-setup-cortexa72-cortexa53-poky-linux
--- scripts/crosscompileuser.sh
+++ scripts/crosscompileuser.sh
@@ -1,10 +1,2 @@
-export ARCH=arm64
-export CROSS_COMPILE=aarch64-linux-gnu-
-export CFLAGS="-O2 -pipe -g -feliminate-unused-debug-types"
-export LDFLAGS="-Wl,-O1 -Wl,--hash-style=gnu -Wl,--as-needed"
-export CXXFLAGS="-O2 -pipe -g -feliminate-unused-debug-types"
-export CPP="aarch64-linux-gnu-gcc -E --sysroot=/home/embedded/FS"
-export CC="aarch64-linux-gnu-gcc --sysroot=/home/embedded/FS"
-export CXX="aarch64-linux-gnu-g++ --sysroot=/home/embedded/FS"
-export LD="aarch64-linux-gnu-ld --sysroot=/home/embedded/FS"
-export CONFIGURE_FLAGS="--target=aarch64-linux-gnu --host=aarch64-linux-gnu --build=x86_64-linux --with-libtool-sysroot=/home/embedded/FS"
+export KERNEL_VERSION="5.10.137"
+source $HOME/SDK/environment-setup-cortexa72-cortexa53-poky-linux
